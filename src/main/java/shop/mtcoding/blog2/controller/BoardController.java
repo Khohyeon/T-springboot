e862dbadad5eb8ca8750c555dd13284ac52e4e96
kim ho hyeon <khh5762@naver.com>
2023-02-09 17:36:51 +0900

    업데이트폼 수정 권한 까지 완료

--- src/main/java/shop/mtcoding/blog2/controller/BoardController.java
+++ src/main/java/shop/mtcoding/blog2/controller/BoardController.java
@@ -2,7 +2,10 @@ package shop.mtcoding.blog2.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +15,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import shop.mtcoding.blog2.dto.board.BoardReq.BoardSaveReqDto;
 import shop.mtcoding.blog2.dto.board.BoardResp.BoardDetailRespDto;
 import shop.mtcoding.blog2.dto.board.BoardResp.BoardMainRespDto;
+import shop.mtcoding.blog2.handler.ex.CustomException;
+import shop.mtcoding.blog2.model.Board;
 import shop.mtcoding.blog2.model.BoardRepository;
+import shop.mtcoding.blog2.model.User;
 import shop.mtcoding.blog2.service.BoardService;
 
 @Controller
@@ -23,6 +29,9 @@ public class BoardController {
 
     @Autowired
     private BoardRepository boardRepository;
+
+    @Autowired
+    private HttpSession session;
 
     @GetMapping({ "/", "/main" })
     public String main(Model model) {
@@ -39,7 +48,6 @@ public class BoardController {
     @PostMapping("/board")
     public String save(BoardSaveReqDto boardSaveReqDto, Model model) {
         boardService.글쓰기(boardSaveReqDto);
-
         return "redirect:/";
     }
 
@@ -50,8 +58,20 @@ public class BoardController {
         return "board/detail";
     }
 
-    @GetMapping("board/updateForm")
-    public String updateForm() {
+    @GetMapping("/board/{id}/updateForm")
+    public String updateForm(@PathVariable int id, Model model) {
+        User principal = (User) session.getAttribute("principal");
+        if (principal == null) {
+            throw new CustomException("인증이 되지 않았습니다", HttpStatus.UNAUTHORIZED);
+        }
+        Board boardPS = boardRepository.findById(id);
+        if (boardPS == null) {
+            throw new CustomException("없는 게시글을 수정할 수 없습니다.");
+        }
+        if (boardPS.getUserId() != principal.getId()) {
+            throw new CustomException("해당 게시글을 수정할 권한이 없습니다.", HttpStatus.FORBIDDEN);
+        }
+        model.addAttribute("board", boardPS); // 수정해야댐
         return "board/updateForm";
     }
 
--- src/test/java/shop/mtcoding/blog2/controller/BoardControllerTest.java
+++ src/test/java/shop/mtcoding/blog2/controller/BoardControllerTest.java
@@ -1,8 +1,19 @@
 package shop.mtcoding.blog2.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,6 +28,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import shop.mtcoding.blog2.dto.board.BoardReq.BoardSaveReqDto;
+import shop.mtcoding.blog2.dto.board.BoardResp;
+import shop.mtcoding.blog2.model.User;
 
 @Transactional // 메서드 실행 직후에 롤백!
 @AutoConfigureMockMvc
@@ -31,22 +44,109 @@ public class BoardControllerTest {
     @Autowired
     private ObjectMapper om;
 
+    // @BeforeAll
+    // public static void 테이블차리기() {
+    // // 테이블 차리기
+    // }
+
+    // @AfterAll
+    // public void teardown() {
+    // // 데이터 지우고, 다시 인서트
+    // }
+
+    @BeforeEach // Test 메서드 실행 직전 마다에 호출됨.
+    public void setUp() {
+        // 데이터 인서트
+        User user = new User();
+        user.setId(1);
+        user.setUsername("ssar");
+        user.setPassword("1234");
+        user.setEmail("ssar@nate.com");
+        user.setCreatedAt(Timestamp.valueOf(LocalDateTime.now()));
+
+        mockSession = new MockHttpSession();
+        mockSession.setAttribute("principal", user);
+    }
+
+    @Test
+    public void delete_test() throws Exception {
+        // given
+        int id = 1;
+
+        // when
+        ResultActions resultActions = mvc.perform(delete("/board/" + id).session(mockSession));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+        /*
+         * jsonPath
+         * 최상위 : $
+         * 객체탐색 : 닷(.)
+         * 배열 : [0] 번지수 찾기
+         */
+        // then
+        resultActions.andExpect(jsonPath("$.code").value(1));
+        resultActions.andExpect(status().isOk());
+    }
+
+    @Test
+    public void detail_test() throws Exception {
+        // given
+        int id = 1;
+        // when
+        ResultActions resultActions = mvc.perform(
+                get("/board/" + id));
+        Map<String, Object> map = resultActions.andReturn().getModelAndView().getModel();
+        BoardResp.BoardDetailRespDto dto = (BoardResp.BoardDetailRespDto) map.get("dto");
+        String model = om.writeValueAsString(dto);
+        System.out.println("테스트 : size : " + model);
+
+        // then
+        resultActions.andExpectAll(status().isOk());
+        Assertions.assertThat(status().isOk());
+        assertThat(dto.getUsername()).isEqualTo("ssar");
+        assertThat(dto.getId()).isEqualTo(1);
+        assertThat(dto.getTitle()).isEqualTo("첫 번째 글입니다.");
+
+    }
+
+    @Test
+    public void main_test() throws Exception {
+        // given
+        // when
+        ResultActions resultActions = mvc.perform(
+                get("/"));
+        Map<String, Object> map = resultActions.andReturn().getModelAndView().getModel();
+        List<BoardResp.BoardMainRespDto> dtos = (List<BoardResp.BoardMainRespDto>) map.get("dtos");
+        System.out.println("테스트 : size : " + dtos.size());
+        String model = om.writeValueAsString(dtos);
+        System.out.println("테스트 : size : " + model);
+
+        // then
+        resultActions.andExpectAll(status().isOk());
+        Assertions.assertThat(status().isOk());
+        assertThat(dtos.size()).isEqualTo(6);
+        assertThat(dtos.get(0).getUsername()).isEqualTo("ssar");
+        assertThat(dtos.get(0).getTitle()).isEqualTo("첫 번째 글입니다.");
+
+    }
+
     @Test
     public void save_test() throws Exception {
+        // given
         BoardSaveReqDto boardSaveReqDto = new BoardSaveReqDto();
         boardSaveReqDto.setTitle("제목");
         boardSaveReqDto.setContent("내용");
 
-        String responseBody = om.writeValueAsString(boardSaveReqDto);
-
+        String requestBody = om.writeValueAsString(boardSaveReqDto);
+        // when
         ResultActions resultActions = mvc.perform(
                 post("/board")
-                        .content(responseBody)
+                        .content(requestBody)
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
                         .session(mockSession));
 
         System.out.println("save_test : ");
-
+        // then
         resultActions.andExpect(status().isCreated());
     }
 }
